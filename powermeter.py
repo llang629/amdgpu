c13a5b4664bb0c24afc70362da02cdba6df4b9da
Larry Lang <llang@rf-pixels.com>
2021-12-12 09:08:43 -0800

    arrange with proper if_main

--- powermeter.py
+++ powermeter.py
@@ -1,84 +1,97 @@
 """Record miner power usage."""
-import sys
-import signal
 import datetime
+import signal
+import sys
 import time
+
 import requests
 from google.cloud import storage
 
-try:
-    fh = open(sys.argv[1], "w")
-    print("Logging to file", sys.argv[1])
-    sys.stdout = fh
-    sys.stderr = fh
-except (IndexError):
-    print("Logging to console")
-
-
-def signal_handler(signum, frame):
-    """Catch system signals."""
-    # TODO: catch signal from Windows Task Scheduler
-    # TODO: close Google Cloud Storage file gracefully
-    print("Signal Number:", signum, " Frame: ", frame)
-    sys.stdout.flush()
-    sys.exit(signum)
-
-
-catchable_signals = set(signal.Signals)
-if sys.platform == "darwin":
-    catchable_signals -= {signal.SIGKILL, signal.SIGSTOP}
-elif sys.platform == "win32":
-    catchable_signals -= {signal.CTRL_C_EVENT, signal.CTRL_BREAK_EVENT}
-else:
-    print("Untested system")
-    sys.stdout.flush()
-    sys.exit(1)
-
-for sig in catchable_signals:
-    print("Catching", sig)
-    sys.stdout.flush()
-    signal.signal(sig, signal_handler)
-
+storage_client = storage.Client.from_service_account_json("powermeter.json")
+bucket_name = "ethereum_power_test"
+bucket = storage_client.bucket(bucket_name)
 
 miner_url = "http://capitola.larrylang.net/api/eth/gpu"
 interval = 5  # minutes
 
-storage_client = storage.Client.from_service_account_json("powermeter.json")
-bucket_name = "ethereum_power"
-bucket = storage_client.bucket(bucket_name)
 
-previous_epoch = -1  # first pass force open
-while True:
-    date_now_iso = datetime.datetime.now().replace(microsecond=0).isoformat()
-    epoch = datetime.datetime.now().hour  # .minute .day
-    if epoch != previous_epoch:
-        try:
-            gcs_file.close()
-        except NameError:
-            pass  # first pass nothing to close
-        blob = bucket.blob("records-starting-" + date_now_iso)
-        gcs_file = blob.open("wt", content_type="text/csv")
-        print(date_now_iso, "starting new file")
+class SysSignals:
+    """Catch system signals."""
+
+    def __init__(self):
+        """Prepare to catch signals from local operating system."""
+        catchable_signals = set(signal.Signals)
+        if sys.platform == "darwin":
+            catchable_signals -= {signal.SIGKILL, signal.SIGSTOP}
+        elif sys.platform == "win32":
+            catchable_signals -= {signal.CTRL_C_EVENT, signal.CTRL_BREAK_EVENT}
+        else:
+            print("Untested system")
+            sys.stdout.flush()
+            sys.exit(1)
+
+        for sig in catchable_signals:
+            print("Catching", sig)
+            sys.stdout.flush()
+            signal.signal(sig, self.handler)
+
+    def handler(self, signum, frame):
+        """Handle system signals."""
+        # TODO: catch signal from Windows Task Scheduler
+        # TODO: close Google Cloud Storage file gracefully
+        print("Signal Number:", signum, " Frame: ", frame)
         sys.stdout.flush()
-        previous_epoch = epoch
-    gpu_count = 0
-    gpu_power = 0
+        sys.exit(signum)
+
+
+def record(url, interval):
+    """Record GPU power consumption every interval."""
+    previous_epoch = -1  # first pass force open
+    while True:
+        date_now_iso = datetime.datetime.now().replace(
+            microsecond=0).isoformat()
+        epoch = datetime.datetime.now().hour  # .minute .day
+        if epoch != previous_epoch:
+            try:
+                gcs_file.close()
+            except NameError:
+                pass  # first pass nothing to close
+            blob = bucket.blob("records-starting-" + date_now_iso)
+            gcs_file = blob.open("wt", content_type="text/csv")
+            print(date_now_iso, "starting new file")
+            sys.stdout.flush()
+            previous_epoch = epoch
+        gpu_count = 0
+        gpu_power = 0
+        try:
+            gpus = requests.get(url=url, timeout=10).json()["DEVS"]
+            gpu_count = len(gpus)
+            for gpu in gpus:
+                gpu_power += gpu["GPU Power"]
+        except (requests.exceptions.RequestException, ValueError):
+            pass  # api fails to respond or response json invalid
+        record = date_now_iso + ","
+        record += str(gpu_count) + ","
+        record += str(gpu_power)
+        print(record)
+        sys.stdout.flush()
+        gcs_file.write(record + "\n")
+        try:
+            sleep_duration = 60 * (interval - datetime.datetime.now().minute %
+                                   interval) - datetime.datetime.now().second
+        except ZeroDivisionError:
+            sleep_duration = 10
+        time.sleep(sleep_duration)
+
+
+if __name__ == "__main__":
     try:
-        gpus = requests.get(url=miner_url, timeout=10).json()["DEVS"]
-        gpu_count = len(gpus)
-        for gpu in gpus:
-            gpu_power += gpu["GPU Power"]
-    except (requests.exceptions.RequestException, ValueError):
-        pass  # api fails to respond or response json invalid
-    record = date_now_iso + ","
-    record += str(gpu_count) + ","
-    record += str(gpu_power)
-    print(record)
-    sys.stdout.flush()
-    gcs_file.write(record + "\n")
-    try:
-        sleep_duration = 60 * (interval - datetime.datetime.now().minute %
-                               interval) - datetime.datetime.now().second
-    except ZeroDivisionError:
-        sleep_duration = 10
-    time.sleep(sleep_duration)
+        fh = open(sys.argv[1], "w")
+        print("Logging to file", sys.argv[1])
+        sys.stdout = fh
+        sys.stderr = fh
+    except (IndexError):
+        print("Logging to console")
+
+    syssig = SysSignals()
+    record(miner_url, interval)
--- powertool.py
+++ powertool.py
@@ -1,40 +1,56 @@
-"""Process miner power usage records."""
+"""Process miner power usage records on Google Cloud Storage."""
 import datetime
 from itertools import chain
+
 from google.cloud import storage
+
+import powermeter
 
 storage_client = storage.Client.from_service_account_json("powermeter.json")
 bucket_name = "ethereum_power"
 bucket = storage_client.bucket(bucket_name)
 
+interval = powermeter.interval
+price_kwh = 0.23
 
-def consolidate(name, sources=[]):
-    if not name:
+
+def list(blobprefix):
+    """List GCS blobs with prefix."""
+    return storage_client.list_blobs(bucket_name, prefix=blobprefix)
+
+
+def compose(blobname, sources=[]):
+    """Compose new GCS blob from list of source blobs."""
+    if not blobname:
         return
-    print(color.green + u'\u2192' + color.end,
-          color.underline + name + color.end)
-    destination = bucket.blob(name)
+    print(TTY.green + u'\u2192' + TTY.end, TTY.underline + blobname + TTY.end)
+    destination = bucket.blob(blobname)
     destination.content_type = "text/csv"
     destination.compose(sources)
 
 
-class color:
+class TTY:
+    """ASCII terminal escape sequences."""
+
     cls = '\033c'
-    red = '\033[91m'
-    yellow = '\033[93m'
-    green = '\033[92m'
-    blue = '\033[94m'
     bold = '\033[1m'
     underline = '\033[4m'
+    red = '\033[91m'
+    green = '\033[92m'
+    blue = '\033[94m'
     end = '\033[0m'
 
 
-class Level:
+class LevelStep:
+    """Step from one interval to the next-longer interval."""
+
     def __init__(self, message, prefix):
+        """Provide interface message and blob prefix for each interval."""
         self.message = message
         self.prefix = prefix
 
     def period(self, name):
+        """Format blobname for next-longer interval."""
         if self.prefix == "records-starting":
             return name.split("T")[0].replace("starting", "daily")
         elif self.prefix == "records-daily":
@@ -45,60 +61,73 @@ class Level:
             return None
 
 
-hour2day = Level("Hourly into daily", "records-starting")
-day2month = Level("Daily into monthly", "records-daily")
-month2year = Level("Monthly into annual", "records-monthly")
-levels = [hour2day, day2month, month2year]
-print(color.cls + color.bold + "*** Consolidating records ***" + color.end)
-for level in levels:
-    previous = None
-    sources = []
-    print(color.blue + level.message + "..." + color.end)
-    for blob in storage_client.list_blobs(bucket_name, prefix=level.prefix):
-        period = level.period(blob.name)
-        if period != previous:
-            consolidate(previous, sources)
-            previous = period
-            sources = [bucket.get_blob(blob.name)]
-            print(u'\u2713', blob.name)
+def consolidate():
+    """Consolidate records into longer intervals."""
+    hour2day = LevelStep("Hourly into daily", "records-starting")
+    day2month = LevelStep("Daily into monthly", "records-daily")
+    month2year = LevelStep("Monthly into annual", "records-monthly")
+    levels = [hour2day, day2month, month2year]
+    print(TTY.cls + TTY.bold + "*** Consolidating records ***" + TTY.end)
+    for level in levels:
+        previous = None
+        sources = []
+        print(TTY.blue + level.message + "..." + TTY.end)
+        for blob in list(level.prefix):
+            period = level.period(blob.name)
+            if period != previous:
+                compose(previous, sources)
+                previous = period
+                sources = [bucket.get_blob(blob.name)]
+                print(u'\u2713', blob.name)
+            else:
+                sources.append(bucket.get_blob(blob.name))
+                print(u'\u2713', blob.name)
         else:
-            sources.append(bucket.get_blob(blob.name))
-            print(u'\u2713', blob.name)
+            compose(previous, sources)
+
+
+def delete():
+    """Delete hourly and daily records from previous months."""
+    month_now = datetime.datetime.now().month
+    print(TTY.bold + "*** Gathering older records to delete ***" + TTY.end)
+    blobs_to_delete = []
+    for blob in chain(list("records-starting"), list("records-daily")):
+        if int(blob.name.split("-")[3]) == month_now:
+            continue
+        print(u'\u274C', blob.name)
+        blobs_to_delete.append(bucket.get_blob(blob.name))
+
+    if blobs_to_delete:
+        confirm = input(TTY.red + "Confirm deletions (y/N)? " + TTY.end)
+        if confirm.lower() == "y":
+            bucket.delete_blobs(blobs_to_delete)
+            print("Deletions completed")
+        else:
+            print("Deletions cancelled")
     else:
-        consolidate(previous, sources)
+        print("Nothing to delete")
 
-month_now = datetime.datetime.now().month
-print(color.bold + "*** Gathering older records to delete ***" + color.end)
-blobs_to_delete = []
-for blob in chain(
-        storage_client.list_blobs(bucket_name, prefix="records-starting"),
-        storage_client.list_blobs(bucket_name, prefix="records-daily")):
-    if int(blob.name.split("-")[3]) == month_now:
-        continue
-    print(u'\u274C', blob.name)
-    blobs_to_delete.append(bucket.get_blob(blob.name))
 
-if blobs_to_delete:
-    confirm = input(color.red + "Confirm deletions (y/N)? " + color.end)
-    if confirm.lower() == "y":
-        bucket.delete_blobs(blobs_to_delete)
-        print("Deletions completed")
-    else:
-        print("Deletions cancelled")
-else:
-    print("Nothing to delete")
+def energy(interval, price):
+    """Calculate energy use and cost.
 
-price = 0.23  # price per kWh in dollars
-interval = 5  # duration of record interval in minutes
-# TODO: import interval from powermeter
-print(color.bold +
-      "*** Calculating energy use and costs (${:0.2f} per kWh) ***".format(
-          price) + color.end)
-for blob in storage_client.list_blobs(bucket_name, prefix="records-monthly"):
-    monthly_records = blob.download_as_string().splitlines()
-    watts = 0
-    for record in monthly_records:
-        watts += float(record.split(b",")[2])
-    kwh = watts * (interval / 60) / 1000
-    cost = kwh * price
-    print(blob.name, "\t{:0.4f} kWh\t${:0.2f}".format(kwh, cost))
+    interval: record duration in minutes
+    price: per kWh in dollars
+    """
+    print(TTY.bold +
+          "*** Calculating energy use and costs at ${:0.2f} per kWh ***".
+          format(price) + TTY.end)
+    for blob in list("records-monthly"):
+        monthly_records = blob.download_as_string().splitlines()
+        watts = 0
+        for record in monthly_records:
+            watts += float(record.split(b",")[2])
+        kwh = watts * (interval / 60) / 1000
+        cost = kwh * price
+        print(blob.name, "\t{:0.4f} kWh\t${:0.2f}".format(kwh, cost))
+
+
+if __name__ == "__main__":
+    consolidate()
+    delete()
+    energy(interval, price_kwh)
